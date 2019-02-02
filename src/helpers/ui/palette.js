8a4da7e7f379521b82f7fd8faaf92cbb95eec27e
ANDREW SEGAL <segal.a@hotmail.com>
2019-02-02 13:33:53 -0500

    Make initial palette

---
+++ src/helpers/ui/palette.js
@@ -0,0 +1,20 @@
+import blue from '@material-ui/core/colors/blue';
+import red from '@material-ui/core/colors/red';
+
+export const palette = {
+  palette: {
+    primary: {
+      light: blue[100],
+      main: blue[500],
+      dark: blue[700],
+     },
+    secondary: {
+      light: red[100],
+      main: red[300],
+      dark: red[700],
+       }
+  },
+  status: {
+    danger: 'orange',
+  },
+};
