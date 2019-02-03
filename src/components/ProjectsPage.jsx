8c38d19af67284e3cb4e4d5fed45dd3de6cc2539
ANDREW SEGAL <segal.a@hotmail.com>
2019-02-03 13:12:19 -0500

    Rename and implement routing for Projects Page

---
+++ src/components/ProjectsPage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import ProjectsTable from './ProjectsTable'
+
+class ProjectsPage extends Component {
+  render() {
+    return (
+      <ProjectsTable />
+    )
+  }
+}
+
+export default ProjectsPage;
