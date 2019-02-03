9dc39655c5800eea9becbc514ed9d8942735e460
ANDREW SEGAL <segal.a@hotmail.com>
2019-02-03 16:11:09 -0500

    Add basic navigation to NavBar

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import ArrowBack from '@material-ui/icons/ArrowBackRounded'
+import Home from '@material-ui/icons/HomeRounded'
 
 const styles = {
   root: {
@@ -11,24 +15,34 @@ const styles = {
   },
 };
 
-function SimpleAppBar(props) {
-  const { classes } = props;
+class SimpleAppBar extends Component {
+  render () {
+    const { classes } = this.props;
+    const handleHome = () => this.props.history.push('/')
+    const goBack = (props) => {
+      this.props.history.goBack();
+    };
 
-  return (
-    <div className={classes.root}>
-      <AppBar position="static" color="primary">
-        <Toolbar>
-          <Typography variant="h6" style={{'color': 'white'}}>
-            Bering Mechanical
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
+    return (
+      <div className={classes.root}>
+        <AppBar position="static" color="primary">
+          <Toolbar>
+            { window.location.pathname === '/' ? null : <ArrowBack onClick={goBack} /> }
+            <Typography variant="h6" className={classes.root} style={{'paddingLeft': '1rem', 'color': 'white'}}>
+              Bering Mechanical
+            </Typography>
+            <IconButton color="inherit" onClick={handleHome} >
+              <Home />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
 }
 
 SimpleAppBar.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(SimpleAppBar);
+export default withRouter(withStyles(styles)(SimpleAppBar));
